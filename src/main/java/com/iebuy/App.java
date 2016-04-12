cea44441224caef16eeb4d6d61516d480345b9f5
qys0730 <qys0730@163.com>
2016-04-12 21:28:53 +0800

    Signed-off-by: qys0730 <qys0730@163.com>

---
+++ src/main/java/com/iebuy/App.java
@@ -0,0 +1,13 @@
+package com.iebuy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
